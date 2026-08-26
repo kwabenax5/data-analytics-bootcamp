5eef79184756c2d167d863f490b2087006c613e8
Kwabena Afful <kwabenaafful5@gmail.com>
2026-08-26 22:35:09 +0000

    Add advanced SQL sales analysis

--- 04. Project/Customer Sales Performance/customer_sales_analysis.sql
+++ 04. Project/Customer Sales Performance/customer_sales_analysis.sql
@@ -111,3 +111,86 @@ JOIN pizza_types pt
     ON p.pizza_type_id = pt.pizza_type_id
 GROUP BY pt.category
 ORDER BY category_revenue DESC;
+
+
+
+
+
+--Which products generate most of the company’s revenue?--
+
+-- 6. Cumulative Revenue (Pareto Analysis)
+
+
+WITH pizza_revenue AS (
+    SELECT
+        pt.name AS pizza_name,
+        SUM(od.quantity * p.price) AS total_revenue
+    FROM order_details od
+    JOIN pizzas p
+        ON od.pizza_id = p.pizza_id
+    JOIN pizza_types pt
+        ON p.pizza_type_id = pt.pizza_type_id
+    GROUP BY pt.name
+)
+
+SELECT
+    pizza_name,
+    total_revenue,
+
+    -- Running total of revenue
+    SUM(total_revenue) OVER (
+        ORDER BY total_revenue DESC
+    ) AS cumulative_revenue,
+
+    -- Percentage of total revenue
+    ROUND(
+        SUM(total_revenue) OVER (
+            ORDER BY total_revenue DESC
+        ) /
+        SUM(total_revenue) OVER () * 100,
+        2
+    ) AS cumulative_percentage
+
+FROM pizza_revenue
+ORDER BY total_revenue DESC;
+
+
+
+-- 7. Month-over-Month Revenue Growth
+
+WITH revenue_by_month AS (
+    SELECT
+        MONTH(o.order_date) AS order_month,
+        SUM(od.quantity * p.price) AS current_month_sales
+    FROM order_details od
+    JOIN pizzas p
+        ON od.pizza_id = p.pizza_id
+    JOIN orders o
+        ON od.order_id = o.order_id
+    GROUP BY MONTH(o.order_date)
+),
+
+monthly_comparison AS (
+    SELECT
+        order_month,
+        current_month_sales,
+        LAG(current_month_sales)
+            OVER (ORDER BY order_month) AS previous_month_sales
+    FROM revenue_by_month
+)
+
+SELECT
+    order_month,
+    current_month_sales,
+    previous_month_sales,
+
+    current_month_sales - previous_month_sales AS revenue_change,
+
+    ROUND(
+        (current_month_sales - previous_month_sales)
+        / previous_month_sales * 100,
+        2
+    ) AS growth_percentage
+
+FROM monthly_comparison
+ORDER BY order_month;
---
+++ 04. Project/Customer Sales Performance/project_notes.md
@@ -0,0 +1,5 @@
+## Pareto Analysis
+
+- Top 5 pizzas generated **24.53%** of total revenue.
+- Approximately **22 pizzas** accounted for **80%** of cumulative revenue.
+- Revenue is distributed across a broad range of products rather than being concentrated in only a few pizzas.
